01bca782af908370281ec1caea1e88a36406d66d
Yuval Steinhart <yuval@cs.ucsb.edu>
2022-05-02 16:28:36 -0700

    Updated proxy

--- proxy/benchmark.cpp
+++ proxy/benchmark.cpp
@@ -26,7 +26,10 @@ using namespace boost::asio::ip;
 using namespace std::chrono;
 
 
-#define outFilePath "/home/ubuntu/waffle/outFile.txt"
+#define MAX_KEY 10000
+#define VALUE_SIZE 1000
+
+#define outFilePath "./outFile.txt"
 
 int main(int argc, char* argv[])
 {
@@ -37,29 +40,27 @@ int main(int argc, char* argv[])
     }
 
 	std::string operationType;
-	std::string key;
 	std::string value;
 
 	Record* valueRecord;
 
-	CloudDB store("./secretKeys/sodiumKey");
+	CloudDB store("secretKeys/sodiumKey");
 
 	FILE* outFile = fopen(outFilePath, "wb");
 
     float diff;
     std::vector<float> put_times, get_times;
-    char entry[1000];
-    std::string str_entry;
+    std::string key;
+    srand( (unsigned)time( NULL ) );
     for(int i = 0; i < 1000; i++){
-        bzero(entry, 1000);
-        entry[i / 8] |= 1 << i % 8;
-        str_entry = std::string(entry);
+        
+        key = std::string(std::to_string(rand() % MAX_KEY));
         value = "1";
 
 
-        char* valueData = (char*)malloc(value.size() + 1);
+        char* valueData = (char*)malloc(VALUE_SIZE);
         memcpy(valueData, value.c_str(), value.size() + 1);
-        valueRecord = new Record(entry, valueData, value.size() + 1, std::chrono::system_clock::now());
+        valueRecord = new Record(key, valueData, VALUE_SIZE, std::chrono::system_clock::now());
         store.put(valueRecord);
         delete valueRecord;
 
@@ -67,13 +68,17 @@ int main(int argc, char* argv[])
 
         auto start = high_resolution_clock::now();
 
-        valueData = (char*)malloc(value.size() + 1);
+        valueData = (char*)malloc(VALUE_SIZE);
+
         memcpy(valueData, value.c_str(), value.size() + 1);
-        valueRecord = new Record(entry, valueData, value.size() + 1, std::chrono::system_clock::now());
-        store.get(entry);
+        valueRecord = new Record(key, valueData, VALUE_SIZE, std::chrono::system_clock::now());
+
+
+        Record* tmp = store.get(key);
         store.put(valueRecord);
-        delete valueRecord;
         auto stop = high_resolution_clock::now();
+        delete tmp;
+        delete valueRecord;
         put_times.push_back(duration_cast<microseconds>(stop - start).count());
 
         start = high_resolution_clock::now();
@@ -86,6 +91,7 @@ int main(int argc, char* argv[])
         // }
         // else printf("Record not found\n");
         stop = high_resolution_clock::now();
+        delete valueRecord;
         get_times.push_back(duration_cast<microseconds>(stop - start).count());
 
     }
